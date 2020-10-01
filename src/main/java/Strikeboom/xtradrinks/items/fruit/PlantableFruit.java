3a2ba5fb8db9e2ee1fa085b1da51b05f95fabe2d
IStrikeboomI <shubhmakadia12@gmail.com>
2020-10-01 17:51:11 -0500

    Changed an extend ItemFood implements IPlantable to ItemSeedFood

--- src/main/java/Strikeboom/xtradrinks/items/fruit/PlantableFruit.java
+++ src/main/java/Strikeboom/xtradrinks/items/fruit/PlantableFruit.java
@@ -1,47 +1,11 @@
 package Strikeboom.xtradrinks.items.fruit;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemFood;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
-import net.minecraft.world.World;
-import net.minecraftforge.common.EnumPlantType;
-import net.minecraftforge.common.IPlantable;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemSeedFood;
 
-public class PlantableFruit extends ItemFood implements IPlantable {
-    Block block;
+public class PlantableFruit extends ItemSeedFood {
     public PlantableFruit(Block block) {
-        super(1,0.3f,false);
-        this.block = block;
-    }
-
-    @Override
-    public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-        IBlockState state = worldIn.getBlockState(pos);
-        if (facing == EnumFacing.UP && player.canPlayerEdit(pos.offset(facing), facing, player.getHeldItemMainhand()) && state.getBlock().canSustainPlant(state, worldIn, pos, EnumFacing.UP, this) && worldIn.isAirBlock(pos.up()))
-        {
-            worldIn.setBlockState(pos.up(), getPlant(worldIn,pos));
-            player.getHeldItemMainhand().shrink(1);
-            return EnumActionResult.SUCCESS;
-        }
-        else
-        {
-            return EnumActionResult.FAIL;
-        }
-    }
-
-    @Override
-    public EnumPlantType getPlantType(IBlockAccess world, BlockPos pos) {
-        return EnumPlantType.Crop;
-    }
-
-    @Override
-    public IBlockState getPlant(IBlockAccess world, BlockPos pos) {
-        return block.getDefaultState();
+        super(1,0.3f,block, Blocks.FARMLAND);
     }
 }
