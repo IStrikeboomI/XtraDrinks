c33367d4eedac0aa3059f1ea2f7ed2eceb2e18de
Shubh Makadia <60586926+IStrikeboomI@users.noreply.github.com>
2022-06-30 15:31:10 -0500

    Got entity To render

--- src/main/java/Strikeboom/xtradrinks/client/render/entity/GreenmanRenderer.java
+++ src/main/java/Strikeboom/xtradrinks/client/render/entity/GreenmanRenderer.java
@@ -2,25 +2,23 @@ package Strikeboom.XtraDrinks.client.render.entity;
 
 import Strikeboom.XtraDrinks.XtraDrinks;
 import Strikeboom.XtraDrinks.entity.GreenmanEntity;
-import net.minecraft.client.renderer.culling.ClippingHelper;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.entity.layers.CrossedArmsItemLayer;
+import net.minecraft.client.renderer.entity.layers.HeadLayer;
 import net.minecraft.client.renderer.entity.model.VillagerModel;
 import net.minecraft.util.ResourceLocation;
 
 public class GreenmanRenderer extends MobRenderer<GreenmanEntity, VillagerModel<GreenmanEntity>> {
 
     public GreenmanRenderer(EntityRendererManager p_174304_) {
-        super(p_174304_, new VillagerModel<>(0), 1f);
+        super(p_174304_, new VillagerModel<>(0), .5f);
+        this.addLayer(new HeadLayer<>(this));
+        this.addLayer(new CrossedArmsItemLayer<>(this));
     }
 
     @Override
     public ResourceLocation getTextureLocation(GreenmanEntity pEntity) {
         return new ResourceLocation(XtraDrinks.MOD_ID,"textures/entity/greenman.png");
-    }
-
-    @Override
-    public boolean shouldRender(GreenmanEntity pLivingEntity, ClippingHelper pCamera, double pCamX, double pCamY, double pCamZ) {
-        return super.shouldRender(pLivingEntity, pCamera, pCamX, pCamY, pCamZ);
     }
 }
