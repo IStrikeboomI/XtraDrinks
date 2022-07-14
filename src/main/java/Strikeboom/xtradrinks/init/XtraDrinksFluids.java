2240909eaed10870789a144542b13cec23e4b071
Shubh Makadia <60586926+IStrikeboomI@users.noreply.github.com>
2022-07-13 21:21:05 -0500

    Removed an extra parameter

--- src/main/java/Strikeboom/xtradrinks/init/XtraDrinksFluids.java
+++ src/main/java/Strikeboom/xtradrinks/init/XtraDrinksFluids.java
@@ -23,15 +23,15 @@ public class XtraDrinksFluids {
     public static final DeferredRegister<Fluid> FLUIDS = DeferredRegister.create(ForgeRegistries.FLUIDS, XtraDrinks.MOD_ID);
     public static final DeferredRegister<FluidType> FLUID_TYPES = DeferredRegister.create(ForgeRegistries.Keys.FLUID_TYPES, XtraDrinks.MOD_ID);
 
-    public static final RegistryObject<FluidType> MOLTEN_FIZZIUM_TYPE = registerFluidType("molten_fizzium",new ResourceLocation(XtraDrinks.MOD_ID,"block/molten_fizzium_still"),new ResourceLocation(XtraDrinks.MOD_ID,"block/molten_fizzium_flowing"),new ResourceLocation(XtraDrinks.MOD_ID,"block/molten_fizzium_flowing"),0xfaaa39);
+    public static final RegistryObject<FluidType> MOLTEN_FIZZIUM_TYPE = registerFluidType("molten_fizzium",new ResourceLocation(XtraDrinks.MOD_ID,"block/molten_fizzium_still"),new ResourceLocation(XtraDrinks.MOD_ID,"block/molten_fizzium_flowing"),0xfaaa39);
     public static final RegistryObject<Fluid> MOLTEN_FIZZIUM = FLUIDS.register("molten_fizzium",() -> new ForgeFlowingFluid.Source(fizziumProperties()));
     public static final RegistryObject<Fluid> MOLTEN_FIZZIUM_FLOWING = FLUIDS.register("molten_fizzium_flowing",() -> new ForgeFlowingFluid.Flowing(fizziumProperties()));
     
-    public static final RegistryObject<FluidType> MOLTEN_LIQUADIUM_TYPE = registerFluidType("molten_liquadium",new ResourceLocation(XtraDrinks.MOD_ID,"block/molten_liquadium_still"),new ResourceLocation(XtraDrinks.MOD_ID,"block/molten_liquadium_flowing"),new ResourceLocation(XtraDrinks.MOD_ID,"block/molten_liquadium_flowing"),0x4fb8e8);
+    public static final RegistryObject<FluidType> MOLTEN_LIQUADIUM_TYPE = registerFluidType("molten_liquadium",new ResourceLocation(XtraDrinks.MOD_ID,"block/molten_liquadium_still"),new ResourceLocation(XtraDrinks.MOD_ID,"block/molten_liquadium_flowing"),0x4fb8e8);
     public static final RegistryObject<Fluid> MOLTEN_LIQUADIUM = FLUIDS.register("molten_liquadium",() -> new ForgeFlowingFluid.Source(liquadiumProperties()));
     public static final RegistryObject<Fluid> MOLTEN_LIQUADIUM_FLOWING = FLUIDS.register("molten_liquadium_flowing",() -> new ForgeFlowingFluid.Flowing(liquadiumProperties()));
 
-    public static final RegistryObject<FluidType> MOLTEN_JUICETANIUM_TYPE = registerFluidType("molten_juicetanium",new ResourceLocation(XtraDrinks.MOD_ID,"block/molten_juicetanium_still"),new ResourceLocation(XtraDrinks.MOD_ID,"block/molten_juicetanium_flowing"),new ResourceLocation(XtraDrinks.MOD_ID,"block/molten_juicetanium_flowing"),0x2ccf17);
+    public static final RegistryObject<FluidType> MOLTEN_JUICETANIUM_TYPE = registerFluidType("molten_juicetanium",new ResourceLocation(XtraDrinks.MOD_ID,"block/molten_juicetanium_still"),new ResourceLocation(XtraDrinks.MOD_ID,"block/molten_juicetanium_flowing"),0x2ccf17);
     public static final RegistryObject<Fluid> MOLTEN_JUICETANIUM = FLUIDS.register("molten_juicetanium",() -> new ForgeFlowingFluid.Source(juicetaniumProperties()));
     public static final RegistryObject<Fluid> MOLTEN_JUICETANIUM_FLOWING = FLUIDS.register("molten_juicetanium_flowing",() -> new ForgeFlowingFluid.Flowing(juicetaniumProperties()));
 
@@ -44,7 +44,7 @@ public class XtraDrinksFluids {
     private static ForgeFlowingFluid.Properties juicetaniumProperties() {
         return new ForgeFlowingFluid.Properties(MOLTEN_JUICETANIUM_TYPE,MOLTEN_JUICETANIUM,MOLTEN_JUICETANIUM_FLOWING).tickRate(30).bucket(XtraDrinksItems.MOLTEN_JUICETANIUM_BUCKET).block(() -> (LiquidBlock) XtraDrinksBlocks.MOLTEN_JUICETANIUM_BLOCK.get());
     }
-    public static RegistryObject<FluidType> registerFluidType(String name, ResourceLocation still, ResourceLocation flowing, ResourceLocation overlay, int tint) {
+    public static RegistryObject<FluidType> registerFluidType(String name, ResourceLocation still, ResourceLocation flowing, int tint) {
         return FLUID_TYPES.register(name, () -> new FluidType(FluidType.Properties.create()
                 .lightLevel(15)
                 .density(2000)
@@ -72,12 +72,6 @@ public class XtraDrinksFluids {
                     public ResourceLocation getFlowingTexture() {
                         return flowing;
                     }
-
-                    @Override
-                    public @Nullable ResourceLocation getOverlayTexture() {
-                        return overlay;
-                    }
-
                 });
             }
         });
